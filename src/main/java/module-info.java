47cea565d2f40772ba3dcae20791bef5a4a18f45
Becanar <benatortubitarte@gmail.com>
2024-11-06 13:18:19 +0100

    Corregido tablaDeportes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,10 @@ module com.example.proyectodein {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql.rowset; // Verifica que esto sea necesario
+    requires javafx.base;
 
+
+    opens com.example.proyectodein.model to javafx.base;
     exports com.example.proyectodein.app;
     opens com.example.proyectodein.app to javafx.fxml;
     exports com.example.proyectodein.controladores;
